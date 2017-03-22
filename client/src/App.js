6876d9505bbe26b12653d945e8662b6456430101
Artem <pstemka@gmail.com>
2017-03-22 22:42:25 +0300

    new client

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,21 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
-   constructor(props) {
-       super(props)
-       this.state = {
-           hurra: 'NOPE'
-       }
-
-   }
-    componentDidMount() {
-        fetch('/api/article')
-            .then((artarray) => {
-                const art1 = JSON.parse(artarray[0])
-                return art1
-        })
-            .then((art1obj) => {
-               this.setState({
-                   hurra: art1obj
-               })
-            }).
-            catch((err) => {
-            alert(err)
-        })
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
-                    <h1>{this.state.hurra}</h1>
-                </div>
-                <p className="App-intro">
-                      To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
 
-export default App
-
+export default App;
