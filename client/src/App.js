853f17e9aafd67a4b12d9e16430c0873299839de
Artem <pstemka@gmail.com>
2017-03-22 23:00:33 +0300

    test fetch 5

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+    state = {
+        hey: 'not yet'
+    }
+
+    componentDidMount() {
+        fetch('api/articles').then((artarray) => {
+            return artarray[0]
+        }).then((art1) => {
+            const artobj = JSON.parse(art1)
+            this.setState({
+                hey: artobj.title
+            })
+        }).catch((err) => {
+            alert(err)
+        })
+    }
+
+    render() {
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +30,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <p>
+              {this.state.hey}
+          </p>
       </div>
     );
   }
