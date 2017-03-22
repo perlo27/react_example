554a73fa395fb5e6408cbfefff562df6831681a9
Artem <pstemka@gmail.com>
2017-03-22 21:25:48 +0300

    try fetch

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,25 @@ import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
+    state = {
+        hurra: ''
+    }
+
+    componentDidMount() {
+        fetch('/api/article')
+            .then((artarray) => {
+                const art1 = JSON.parse(artarray[0])
+                return art1
+        })
+            .then((art1obj) => {
+               this.setState({
+                   hurra: art1obj
+               })
+            }).
+            catch((err) => {
+            alert(err)
+        })
+    }
 
     render() {
         return (
@@ -10,6 +29,7 @@ class App extends Component {
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <h2>Welcome to React</h2>
+                    <h1>{this.state.hurra}</h1>
                 </div>
                 <p className="App-intro">
                       To get started, edit <code>src/App.js</code> and save to reload.
