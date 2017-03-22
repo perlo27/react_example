541412fa6bc1246ff892b0002ac8376008809dbd
Artem <pstemka@gmail.com>
2017-03-22 20:08:20 +0300

    bod

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var express = require('express');
 var api = require('./api');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 3001));
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
 // app.use(bodyParser.json());
-// app.use('/api', api)
+// app.use('/api', api);
 
 app.listen(app.get('port'), 'localhost', function (err) {
     if (err) {
